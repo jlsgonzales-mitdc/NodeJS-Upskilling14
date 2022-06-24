8ea3b39788fa157e18141ba6fd5958d11b559b80
Jaspher Jan Pasol <Jaspher_Jan_Pasol@manulife.com>
2022-06-24 10:35:10 +0800

    added control tips

--- ui/boxes/controls.js
+++ ui/boxes/controls.js
@@ -1,5 +1,5 @@
 const { TerminalBox} = require('../shared'); 
-const { Config } = require('../shared');
+const { Config, Keys } = require('../shared');
 
 class Controls extends TerminalBox {
     constructor() {
@@ -9,14 +9,15 @@ class Controls extends TerminalBox {
 
     setPlaylistTips() {
         this.box.content = `
-            3213123212
+            ${Keys.FOCUS_QUEUE} - focus queue | ${Keys.SCROLL_UP} - go up
+            ${Keys.QUEUE_ADD} - enqueue song | ${Keys.SCROLL_DOWN} - go down
         `;
     }
     setQueueTips() {
         this.box.content = `
-            test 123
-            123
-            123
+            ${Keys.MOVE_UP} - move song up | ${Keys.SCROLL_UP}-go up
+            ${Keys.MOVE_DOWN} - move song down | ${Keys.SCROLL_DOWN}-go down
+            ${Keys.FOCUS_PLAYLIST} - focus playlist | ${Keys.QUEUE_REMOVE} - dequeue son
         `;
     }
 }
--- ui/shared/index.js
+++ ui/shared/index.js
@@ -1,7 +1,8 @@
 const TerminalBox = require('./terminalbox');
 const Config = require('./config');
-
+const Keys = require('./keys');
 module.exports = {
     TerminalBox,
-    Config
+    Config,
+    Keys
 };
