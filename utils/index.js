2d4288166965ab89d1f33042d557d4f53f64e717
Jaspher Jan Pasol <Jaspher_Jan_Pasol@manulife.com>
2022-06-24 11:39:46 +0800

    relocated tracks

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,11 @@
 const Fs = require('fs');
 const { extname } = require('path');
 
-const _readDir = () => Fs.readdirSync(process.cwd(), { withFileTypes: true });
+const _readDir = () => Fs.readdirSync(process.cwd()+'/tracks', { withFileTypes: true });
 const _isMp3 = item => item.isFile && extname(item.name) === '.mp3';
 
-exports.readSong = () => _readDir().filter(_isMp3)[0].name;
-exports.readSongs = () => _readDir().filter(_isMp3).map((songItem) => songItem.name);
+exports.readSong = () => 'tracks/'+_readDir().filter(_isMp3)[0].name;
+exports.readSongs = () => _readDir().filter(_isMp3).map((songItem) => 'tracks/'+songItem.name);
 
 exports.discardFirstWord = str => str.substring(str.indexOf(' ') + 1);
 exports.getFirstWord = str => str.split(' ')[0];
